896048936265924d78488c516bd7efa29fba5b2e
İsmail Can Yıldırım <ismailcan5106@gmail.com>
2022-10-15 16:52:48 +0300

    Add project files.

---
+++ Methods/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Add(2, 5));
+            Console.WriteLine(Add(8, 9));
+            Console.ReadLine();
+
+
+
+        }
+        static int Add(int number1, int number2)
+        {
+
+            return number1 + number2;
+
+
+
+        }
+    }
+}
